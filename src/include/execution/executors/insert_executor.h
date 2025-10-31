35d6661f8e18eb8d65266632236e190ca5cb1ab3
skyfishine <743544510@qq.com>
2025-10-31 07:26:41 +0000

    format: 格式化代码

--- src/include/execution/executors/insert_executor.h
+++ src/include/execution/executors/insert_executor.h
@@ -60,7 +60,7 @@ class InsertExecutor : public AbstractExecutor {
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
   std::unique_ptr<AbstractExecutor> child_executor_;
-  bool is_executed_;
+  bool is_executed_ = {false};
 };
 
 }  // namespace bustub
